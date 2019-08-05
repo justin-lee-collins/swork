09ac1e3cc9d0bf1c95bc0dd6409fecd579618d4e
Justin Collins <connect.with.justin@gmail.com>
2019-08-04 22:36:43 -0500

    added comments and updated configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,13 +1,44 @@
-// tslint:disable-next-line:interface-name
-export interface Configuration {
+interface IConfiguration {
+    version: string;
+    origin: string;
+    environment: "production" | "development";
+}
 
+declare global {
+    // tslint:disable-next-line:interface-name
+    interface WorkerGlobalScope {
+        _swork: IConfiguration;
+    }
+}
+
+if (!self._swork) {
+    self._swork = {
+        environment: "production",
+        origin: self.location.origin,
+        version: "1.0.0",
+    } as IConfiguration;
+}
+
+/**
+ * Contains swork global configurations.
+ *
+ * @class Configuration
+ * @implements {IConfiguration}
+ */
+class Configuration implements IConfiguration {
     /**
      * Defines the version of the service worker.
      *
      * @type {string}
      * @memberof Configuration
      */
-    version: string;
+    public get version() {
+        return self._swork.version;
+    }
+
+    public set version(value: string) {
+        self._swork.version = value;
+    }
 
     /**
      * Defines the origin of the service worker. If not provided,
@@ -17,7 +48,13 @@ export interface Configuration {
      * @type {string}
      * @memberof Configuration
      */
-    origin: string;
+    public get origin(): string {
+        return self._swork.origin;
+    }
+
+    public set origin(value: string) {
+        self._swork.origin = value;
+    }
 
     /**
      * Defines the current build type of the service worker. Supported
@@ -26,11 +63,13 @@ export interface Configuration {
      * @type {("production" | "development")}
      * @memberof Configuration
      */
-    environment: "production" | "development";
+    public get environment(): "production" | "development" {
+        return self._swork.environment;
+    }
+
+    public set environment(value: "production" | "development") {
+        self._swork.environment = value;
+    }
 }
 
-export const configuration = {
-    environment: "production",
-    origin: self.location.origin,
-    version: "1.0.0",
-} as Configuration;
+export const configuration = new Configuration();
